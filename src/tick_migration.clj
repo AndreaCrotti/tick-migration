a59781c8274828d41287473e9219cd81ced44545
Andrea Crotti <andrea.crotti.0@gmail.com>
2021-01-05 13:42:17 +0000

    few more ideas

--- src/tick_migration.clj
+++ src/tick_migration.clj
@@ -16,3 +16,30 @@
 (t/< (t/now)
      (-> (t/now)
          (t/>> (t/new-duration 1 :hours))));; => true
+
+;; formatting dates
+(cf/unparse (cf/formatter "YYYY-MM-DD") (ct/now));; => "2021-01-05"
+(t/format (t/formatter "YYYY-MM-DD") ());; => "2021-01-05"
+
+;; formatting times
+
+;; parsing dates
+
+;; parsing times
+
+;; convert to other types
+
+;; keep both old and new versions at the same time
+
+(def ^:dynamic *use-tick* false)
+
+(defmacro with-tick
+  "Evaluate body setting the tick dynamic"
+  [& body]
+  `(binding [*use-tick* true]
+     ~@body))
+
+(defn with-both-versions [t]
+  (binding [*use-tick* false]
+    (t))
+  (with-tick (t)))
