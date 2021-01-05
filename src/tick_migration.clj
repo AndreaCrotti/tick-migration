8a974ce71e7d2e2b14d8d2a3037eefffd0474329
Andrea Crotti <andrea.crotti.0@gmail.com>
2021-01-05 11:01:15 +0000

    use more readable alias

--- src/tick_migration.clj
+++ src/tick_migration.clj
@@ -1,18 +1,18 @@
 (ns tick-migration
-  (:require [tick.alpha.api :as tick]
-            [clj-time.core :as t]
-            [clj-time.coerce :as tc]
-            [clj-time.format :as tf]))
+  (:require [tick.alpha.api :as t]
+            [clj-time.core :as ct]
+            [clj-time.coerce :as cc]
+            [clj-time.format :as cf]))
 
 ;;; simple time/date functions
-(t/now);; => #clj-time/date-time "2021-01-05T10:25:25.909Z"
-(tick/now);; => #time/instant "2021-01-05T10:25:31.482018Z"
+(ct/now);; => #clj-time/date-time "2021-01-05T10:25:25.909Z"
+(t/now);; => #time/instant "2021-01-05T10:25:31.482018Z"
 
-(t/today);; => #object[org.joda.time.LocalDate 0x56fc39be "2021-01-05"]
-(tick/today);; => #time/date "2021-01-05"
+(ct/today);; => #object[org.joda.time.LocalDate 0x56fc39be "2021-01-05"]
+(t/today);; => #time/date "2021-01-05"
 
 ;; compare timestamps
-(t/before? (t/now) (t/plus (t/now) (t/hours 1)));; => true
-(tick/< (tick/now)
-        (-> (tick/now)
-            (tick/>> (tick/new-duration 1 :hours))));; => true
+(ct/before? (ct/now) (ct/plus (ct/now) (ct/hours 1)));; => true
+(t/< (t/now)
+     (-> (t/now)
+         (t/>> (t/new-duration 1 :hours))));; => true
